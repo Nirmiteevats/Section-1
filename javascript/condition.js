777438b01ad5ce2f8a18995cd0a4ce2249b78121
Nirmiteevats <nirmiteevats31@gmail.com>
2026-06-29 13:30:06 +0530

    javascript day1(basic,conditions,var const let)

---
+++ javascript/condition.js
@@ -0,0 +1,42 @@
+num=40;
+if (num%2===0){
+    console.log('even');
+
+    let msg='hello';
+    const pi=3.14;
+    // pi=3.9876;
+    // console.log(msg);
+
+}else{
+    console.log('odd');
+}
+
+// console.log(msg);
+
+// console.log(1==="1");
+
+
+//program 2
+// check if a person is eligible for driving license
+const age=20;
+if(age>=18){
+    console.log("you are eligible for DL");
+}else{
+    console.log("you are not eligible for DL");
+}
+
+// program 3 yeaahhhhhhhhhh
+
+// check if the triangle is equilateral,isosceles or scalene
+
+const a=30;
+const b=40;
+const c=30;
+
+if(a===b && b==c){
+    console.log("Equilateral triangle");
+} else if(a===b||b===c||a===c){
+    console.log("Isosceles triangle");
+}else{
+    console.log("Scalene triangle");
+}
---
+++ javascript/index.js
@@ -0,0 +1,10 @@
+console.log('Hello World');
+num=45;
+console.log(num);
+console.log(typeof(num));
+myname="nimi";
+console.log(myname);
+console.log(typeof(myname));
+
+isHungry=false;
+console.log(typeof(isHungry));
