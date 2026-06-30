c619a64a6956afe56d41a0b4048d3eb46a078c73
Nirmiteevats <nirmiteevats31@gmail.com>
2026-06-30 11:05:20 +0530

    loops and arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,45 @@
+const arr = ['hello', 27, false, null];
+
+console.log(Array.isArray(arr));
+
+const movies = ['tere naam', 'don', 'obsession', 'dhurandar', 'baazigar', 'chava'];
+
+// indexing
+console.log(movies[3]);
+console.log(movies.indexOf('don'));
+// console.log(movies[-3]);                                //undefined
+console.log(movies.at(-2));                                //negative index                         
+
+movies[2] = 'batman';
+console.log(movies);
+
+// slicing
+console.log(movies.slice(1,4));
+console.log(movies.slice(-3,-1));
+console.log(movies.slice(-3));
+console.log(movies.slice(-3,50));                           //(used in page initiation like in amazon a few pages are shown first)
+
+// adding and removing elements in array
+movies.push('Jungle');      //adds elements at the end of array
+console.log(movies);
+
+movies.unshift('Flash');   //adds elements at the start of array
+console.log(movies);
+
+movies.pop();              //removes element at the end of array
+console.log(movies);
+
+movies.shift();             //removes element at the start of array
+console.log(movies);
+
+// removing elements
+// movies.splice(2,2);   //splice(first index from where to remove, no. of elements to be removed)
+
+// movies.splice(2,2,'superman','aquaman');   //replacing elements
+// replacing elements with as many elements u want
+// splice function- performs many funtions
+
+movies.splice(2,0,'Superman')//insert
+console.log(movies);
+
+
---
+++ javascript/loops.js
@@ -0,0 +1,30 @@
+// program 1
+for (let i = 0; i <= 10; i++) {
+    console.log(i);
+}
+
+// program 2
+//wap to print all odd numbers in range of 10-60
+for (let i = 10; i <= 60; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+
+    }
+}
+
+
+// WHILE LOOP- WHEN NUMBER OF ITERATIONS are not known
+let a = 10;
+
+while (a < 20) {
+    console.log(a);
+    a++;
+};
+
+// do while loop
+let b = 20;
+do {
+    console.log(b);
+    b++;
+
+} while (b < 10);
