aad65ac4998be5856fdb6bedff99d10e56f624e1
Nirmiteevats <nirmiteevats31@gmail.com>
2026-07-12 09:56:42 +0530

    js

--- javascript/arrays.js
+++ javascript/arrays.js
@@ -1,8 +1,11 @@
-const arr = ['hello', 27, false, null];
+const arr = ['hello', 27, false, null];//elements of array can be of any datatype
+//arrays are dynamic in nature, we can add or remove elements from array, they support insertion and deletion
 
 console.log(Array.isArray(arr));
 
 const movies = ['tere naam', 'don', 'obsession', 'dhurandar', 'baazigar', 'chava'];
+
+console.log(movies.length);
 
 // indexing
 console.log(movies[3]);
@@ -13,11 +16,15 @@ console.log(movies.at(-2));                                //negative index
 movies[2] = 'batman';
 console.log(movies);
 
+// movies=[] this will not work
+
 // slicing
 console.log(movies.slice(1,4));
 console.log(movies.slice(-3,-1));
 console.log(movies.slice(-3));
 console.log(movies.slice(-3,50));                           //(used in page initiation like in amazon a few pages are shown first)
+
+
 
 // adding and removing elements in array
 movies.push('Jungle');      //adds elements at the end of array
@@ -31,6 +38,8 @@ console.log(movies);
 
 movies.shift();             //removes element at the start of array
 console.log(movies);
+
+
 
 // removing elements
 // movies.splice(2,2);   //splice(first index from where to remove, no. of elements to be removed)
--- javascript/function.js
+++ javascript/function.js
@@ -1,30 +1,39 @@
-function addNums(a,b){
-    c=a+b;
+function addNums(a, b) {
+    c = a + b;
     console.log(c);
 }
 
-addNums(34,23);
-//console.log(c);
+addNums(34, 23);
+// console.log(c);
 
-const getAvg=function(m1,m2,m3){
-    let avg=(m1+m2+m3)/3;
+const getAvg = function (m1, m2, m3) {
+    let avg = (m1 + m2 + m3) / 3;
 
-    //console.log(avg);
+    // console.log(avg);
     return avg;
-}
+    
+};//best practice
+// getAvg(4,89,23);
 
-const result=getAvg(4,89,23);
+const result = getAvg(4, 89, 23);
 console.log(result);
 
-const factorial=(n)=>{
-    let f=1;
-    
-    for (let i=1;i<=n;i++){
-        f=f*i;
+// 2
+// arrow fuction ----introduced in es6
+const factorial = (n) => {
+    let f = 1;
+
+    for (let i = 1; i <= n; i++) {
+        f = f * i;
 
     }
     return f;
 }
 
-const fact=factorial(6);
+const fact = factorial(7);
 console.log(fact);
+
+// NaN is displayed when an operation is perfoermed on undefined values.
+console.log(5 + undefined);
+console.log(1=='1');
+console.log(1==='1');
